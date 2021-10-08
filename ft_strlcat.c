557d229e01f2b881bf95c2f5edcfe0ac18717837
Percy Jacoby <pjacoby@ge-j3.kzn.21-school.ru>
2021-10-08 19:44:17 +0300

    norm correct ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,33 +6,33 @@
 /*   By: pjacoby <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 18:43:24 by pjacoby           #+#    #+#             */
-/*   Updated: 2021/10/08 18:55:25 by pjacoby          ###   ########.fr       */
+/*   Updated: 2021/10/08 19:43:37 by pjacoby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include "ft_libft.h"
 
-size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	size_t	result;
 
 	result = ft_strlen(dst) + ft_strlen(src);
-    if (dstsize > 0)
-    {
-        while (*dst)
-        {
-            dst++;
-            dstsize--;
-        }
-        while (dstsize > 1 && *src)
-        {
-            *dst = *src;
-            dst++;
-            src++;
-            dstsize--;
-        }
-        *dst = 0;
-    }
+	if (dstsize > 0)
+	{
+		while (*dst)
+		{
+			dst++;
+			dstsize--;
+		}
+		while (dstsize > 1 && *src)
+		{
+			*dst = *src;
+			dst++;
+			src++;
+			dstsize--;
+		}
+		*dst = 0;
+	}
 	return (result);
 }
