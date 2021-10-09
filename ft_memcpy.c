295e1b9841b0844cc3206589b301de1b75eeccff
Percy Jacoby <pjacoby@pr-c5.kzn.21-school.ru>
2021-10-09 14:20:35 +0300

    add ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjacoby <pjacoby@student.21-school.ru      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/09 14:00:00 by pjacoby           #+#    #+#             */
+/*   Updated: 2021/10/09 14:19:22 by pjacoby          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t	i;
+	char	*d;
+	char	*s;
+
+	d = (char *)dst;
+	s = (char *)src;
+	i = 0;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dst);
+}
