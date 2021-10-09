5f9e44a7ea667e45230421666a30575a489c8db4
Percy Jacoby <pjacoby@pr-c5.kzn.21-school.ru>
2021-10-09 13:58:06 +0300

    add ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjacoby <pjacoby@student.21-school.ru      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/09 13:24:22 by pjacoby           #+#    #+#             */
+/*   Updated: 2021/10/09 13:37:44 by pjacoby          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			i;
+	unsigned char	*bstr;
+
+	i = 0;
+	bstr = (unsigned char *)b;
+	if (len > 0)
+		while (i < len)
+			bstr[i++] = c;
+	return (b);
+}
