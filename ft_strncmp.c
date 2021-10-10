e7cc0d97aeeb0d327e66998766138a6e6983791f
pjacoby <pjacoby@sudent.21-school.com>
2021-10-10 21:59:17 +0300

    correct ft_strncmp.c char 2 unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -15,9 +15,13 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+    unsigned char *str1;
+    unsigned char *str2;
 
+    str1 = (unsigned char *)s1;
+    str2 = (unsigned char *)s2;
 	i = 0;
-	while (s1[i] == s2[i] && (i < n - 1))
+	while (str1[i] == str2[i] && (i < n - 1))
 		i++;
-	return (s1[i] - s2[i]);
+	return (str1[i] - str2[i]);
 }
