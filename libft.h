6b3a532c29b99d590ac642e9829448c0ae73f004
pjacoby <pjacoby@sudent.21-school.com>
2021-10-11 00:15:20 +0300

    norm ft_strncmp.c libft.h

--- libft.h
+++ libft.h
@@ -12,6 +12,7 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
@@ -27,11 +28,11 @@ int		ft_tolower(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
-char	*ft_strdup(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
 #endif
