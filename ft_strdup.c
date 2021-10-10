b7cb7769ac509917a709a37360672362464e8ab9
pjacoby <pjacoby@sudent.21-school.com>
2021-10-10 23:52:57 +0300

    add ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjacoby <pjacoby@student.21-school.ru      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 23:51:58 by pjacoby           #+#    #+#             */
+/*   Updated: 2021/10/10 23:52:05 by pjacoby          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <malloc.h>
+
+size_t	ft_strlen(const char *s);
+
+char	*ft_strdup(const char *s)
+{
+	char	*result;
+	int		i;
+
+	i = 0;
+	result = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (result != NULL)
+	{
+		while (s[i])
+		{
+			result[i] = s[i];
+			i++;
+		}
+		result[i] = 0;
+	}
+	return (result);
+}
