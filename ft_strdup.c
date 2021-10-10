8507cd870eb555b09d59bab19bbe1a403e3546ad
pjacoby <pjacoby@sudent.21-school.com>
2021-10-11 00:10:32 +0300

    add ft_strdup.c update libft.h, Makefile

--- ft_strdup.c
+++ ft_strdup.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <malloc.h>
+#include <stdlib.h>
 
 size_t	ft_strlen(const char *s);
 
--- libft.h
+++ libft.h
@@ -32,4 +32,6 @@ void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
+char	*ft_strdup(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
 #endif
