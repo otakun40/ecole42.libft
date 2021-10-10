9a0861ca410a11998fe7863eac3e2a9ea030980e
pjacoby <pjacoby@sudent.21-school.com>
2021-10-10 15:58:56 +0300

    add ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjacoby <pjacoby@student.21-school.ru      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 15:48:54 by pjacoby           #+#    #+#             */
+/*   Updated: 2021/10/10 15:49:38 by pjacoby          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (str1[i] == str2[i] && (i < n - 1))
+		i++;
+	if (str1[i] > str2[i])
+		return (1);
+	if (str1[i] == str2[i])
+		return (0);
+	else
+		return (-1);
+}
