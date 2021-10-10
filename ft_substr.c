bae5375f202d6e02d2f6f9ffa16c40d7cf1aeb07
pjacoby <pjacoby@sudent.21-school.com>
2021-10-11 00:59:28 +0300

    add ft_substr.c, update ft_libft.h

---
+++ ft_substr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjacoby <pjacoby@student.21-school.ru      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 00:58:08 by pjacoby           #+#    #+#             */
+/*   Updated: 2021/10/11 00:58:13 by pjacoby          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substring;
+	int		i;
+	size_t	slen;
+	size_t	substringlen;
+
+	i = 0;
+	slen = ft_strlen(s);
+	if (start + len > slen)
+		substringlen = slen - start;
+	else
+		substringlen = len;
+	substring = (char *)malloc(sizeof(char) * (substringlen + 1));
+	if (substring != NULL)
+	{
+		while (i < substringlen)
+		{
+			substring[i] = s[start];
+			i++;
+			start++;
+		}
+		substring[start] = 0;
+	}
+	return (substring);
+}
--- libft.h
+++ libft.h
@@ -35,4 +35,5 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
